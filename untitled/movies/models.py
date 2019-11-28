d6b8137401eb9b639b5dc265b70a74eeb558ef76
Aika21 <aika21.09.00@gmail.com>
2019-11-28 18:28:46 +0600

    Hey

--- untitled/movies/models.py
+++ untitled/movies/models.py
@@ -6,9 +6,26 @@ from django.db import models
 from django.utils import timezone
 
 class Movie(models.Model):
+    NOT_RATED = 0
+    RATED_G = 1
+    RATED_PG = 2
+    RATED_R = 3
+    RATINGS = (
+        (NOT_RATED,
+         'NR - Not Rated'),
+        (RATED_G,
+         'G - General Audiences'),
+        (RATED_PG,
+         'PG - Parental Guidance'
+         ' Suggested'),
+        (RATED_R, 'R - Restricted'),
+    )
+
     movie_title = models.CharField('Movie title',max_length=200)
 
     movie_imdb = models.FloatField('IMDB score')
+
+    rating = models.IntegerField(choices=RATINGS, default=NOT_RATED)
 
     movie_description = models.TextField('Movie Description')
 
@@ -17,6 +34,7 @@ class Movie(models.Model):
     movie_published_date = models.DateTimeField('published date')
 
     movie_picture_url = models.CharField('movie_picture_url',max_length=200)
+
 
 
     def __str__(self):
